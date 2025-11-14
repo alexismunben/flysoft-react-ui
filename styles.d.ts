b6bce842cacdae2416f18d0ac22a1d6aef8a9154
alexismunben <alexis@munben.com>
2025-11-14 17:48:42 -0300

    color fix

---
+++ styles.d.ts
@@ -0,0 +1,9 @@
+/**
+ * Type declarations for flysoft-react-ui/styles module
+ * This allows TypeScript to recognize CSS imports from the package
+ */
+declare module "flysoft-react-ui/styles" {
+  const styles: string;
+  export default styles;
+}
+
